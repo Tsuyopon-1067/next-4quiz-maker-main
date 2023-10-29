670a4fac9fd78c4852add9477d5d0597e3470af7
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-10-29 22:34:57 +0900

    :sparkles: feat: page turn

--- front/next-4quiz-maker/app/question/page.tsx
+++ front/next-4quiz-maker/app/question/page.tsx
@@ -5,24 +5,45 @@ import React, { useState, useEffect } from "react";
 //import SingleRadioButton from "../components/ui/singleRadioButton";
 import RadioButtonGroup from "../components/ui/radioButtonGroup";
 import HtmlArea from "../components/ui/htmlArea";
+import { json } from 'stream/consumers';
 
 
 export default function Question() {
-  const [nowPage, setNowPage] = useState<number>(1);
+  const [nowPage, setNowPage] = useState<number>(0);
   const [jsonData, setJsonData] = useState(null);
   const [questionUrl, setQuestionUrl] = useState("");
+  const nextPage = () => {
+    if (jsonData != null) {
+      if (nowPage+1 <= jsonData.length) {
+        setNowPage(nowPage+1)
+      }
+    }
+  }
+  const prevPage = () => {
+    if (nowPage >= 2) {
+      setNowPage(nowPage-1)
+    }
+  }
 
   useEffect(() => {
     // ローカルサーバーからJSONデータを取得する
-    fetch('http://localhost:50000/JSON', { method: 'GET' })
-          .then((response) => response.json())
-          .then((data) => {
-            setJsonData(data[nowPage-1]);
-            console.log(data[nowPage-1]);
-            setQuestionUrl("http://localhost:50000/HTML/" + "question/" + data[nowPage-1].file)
-          })
-          .catch((error) => console.error('Error fetching JSON:', error));
-      }, [nowPage]);
+    if (nowPage === 0) {
+      setNowPage(1);
+      fetch('http://localhost:50000/JSON', { method: 'GET' })
+            .then((response) => response.json())
+            .then((data) => {
+              setJsonData(data);
+            })
+            .catch((error) => console.error('Error fetching JSON:', error));
+      }
+    }, []);
+
+  useEffect(() => {
+    // jsonDataが更新された後にsetQuestionUrlを呼び出す
+    if (jsonData != null) {
+      setQuestionUrl("http://localhost:50000/HTML/" + "question/" + jsonData[nowPage-1].file);
+    }
+  }, [jsonData, nowPage]);
 
   if (jsonData == null) {
     return
@@ -35,11 +56,11 @@ export default function Question() {
       <HtmlArea url={questionUrl} />
       <div className={styles.bottom_container}>
         <div className={styles.radio_button_area}>
-          <RadioButtonGroup optionList={jsonData.options} onChange={function (optionNumber: number): void {}} />
+          <RadioButtonGroup optionList={jsonData[nowPage-1].options} onChange={function (optionNumber: number): void {}} />
         </div>
         <div className={styles.arrow_button_area}>
-          <button className={styles.arrow_button}>⇦</button>
-          <button className={styles.arrow_button}>⇨</button>
+          <button className={styles.arrow_button} onClick={prevPage}>⇦</button>
+          <button className={styles.arrow_button} onClick={nextPage}>⇨</button>
         </div>
       </div>
     </main>
