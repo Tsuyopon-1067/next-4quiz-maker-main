14f86b73ee59bdca3308d53e32ea9ad35552484d
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-11-11 08:58:46 +0900

    :pencil2:  fix: file name

--- front/next-4quiz-maker/app/components/ui/resultArea.tsx
+++ front/next-4quiz-maker/app/components/ui/resultArea.tsx
@@ -1,39 +1,44 @@
-'use client';
-import React, { useState, useEffect } from 'react';
-import styles from './resultArea.module.css';
-import MaruBatsu from './MaruBatsu';
+"use client";
+import React, { useState, useEffect } from "react";
+import styles from "./resultArea.module.css";
+import MaruBatsu from "./maruBatsu";
 
 interface ResultAreaProps {
-    num: number
-    selected: string
-    answer: string
-    questionUrl: string
-    answerUrl: string
+    num: number;
+    selected: string;
+    answer: string;
+    questionUrl: string;
+    answerUrl: string;
 }
 
 const ResultArea: React.FC<ResultAreaProps> = (props) => {
-  const { num, selected, answer, questionUrl, answerUrl } = props;
-  return (
-    <div className={styles.card}>
-      <div className={styles.content}>
-        <div className={styles.card_element}>{num}</div>
-        <div className={styles.card_element}><MaruBatsu isCorrect={selected === answer} /></div>
-        <div className={styles.card_element}>{selected}</div>
-        <div className={styles.card_element}>{answer}</div>
-        <div className={styles.card_element}>
-          <div className={styles.button}>
-            <a className={styles.atag} href={questionUrl}>問題</a>
-          </div>
+    const { num, selected, answer, questionUrl, answerUrl } = props;
+    return (
+        <div className={styles.card}>
+            <div className={styles.content}>
+                <div className={styles.card_element}>{num}</div>
+                <div className={styles.card_element}>
+                    <MaruBatsu isCorrect={selected === answer} />
+                </div>
+                <div className={styles.card_element}>{selected}</div>
+                <div className={styles.card_element}>{answer}</div>
+                <div className={styles.card_element}>
+                    <div className={styles.button}>
+                        <a className={styles.atag} href={questionUrl}>
+                            問題
+                        </a>
+                    </div>
+                </div>
+                <div className={styles.card_element}>
+                    <div className={styles.button}>
+                        <a className={styles.atag} href={answerUrl}>
+                            解説
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className={styles.card_element}>
-          <div className={styles.button}>
-            <a className={styles.atag} href={answerUrl}>解説</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
+    );
+};
 
 export default ResultArea;
