4400d19e90b3e8311253d0c03e23f12149c6a987
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-11-11 09:17:07 +0900

    :bug: fix: type error

--- front/next-4quiz-maker/app/result/page.tsx
+++ front/next-4quiz-maker/app/result/page.tsx
@@ -1,87 +1,96 @@
-"use client"
-import Link from 'next/link'
-import styles from './page.module.css'
+"use client";
+import Link from "next/link";
+import styles from "./page.module.css";
 import React, { useState, useEffect } from "react";
 import ResultArea from "../components/ui/resultArea";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 
 interface ResultJson {
-  num: number;
-  Selected: number;
-  Answer: number;
+    num: number;
+    Selected: number;
+    Answer: number;
 }
 
 interface QuizJson {
-  number: number;
-  file: string;
-  answer: number;
-  options: string[];
+    number: number;
+    file: string;
+    answer: number;
+    options: string[];
 }
 
 export default function Result() {
-  const [resultJsonData, setResultJsonData] = useState<ResultJson | null>(null);
-  const [quizJsonData, setQuizJsonData] = useState<QuizJson[] | null>(null);
-  const router = useRouter();
-  const goTitle = () => {
-      router.push('/title');
-  }
+    const [resultJsonData, setResultJsonData] = useState<ResultJson[] | null>(
+        null
+    );
+    const [quizJsonData, setQuizJsonData] = useState<QuizJson[] | null>(null);
+    const router = useRouter();
+    const goTitle = () => {
+        router.push("/title");
+    };
 
-  useEffect(() => {
-    // ローカルサーバーからJSONデータを取得する
-    if (resultJsonData === null) {
-      fetch('http://localhost:50000/GET_RESULT', { method: 'GET' })
-            .then((response) => response.json())
-            .then((data) => {
-              setResultJsonData(data);
-              console.log(data);
-            })
-            .catch((error) => console.error('Error fetching JSON:', error));
-      }
-    if (quizJsonData === null) {
-      fetch('http://localhost:50000/JSON', { method: 'GET' })
-            .then((response) => response.json())
-            .then((data) => {
-              setQuizJsonData(data);
-              console.log("hogehoge");
-              console.log(data);
-            })
-            .catch((error) => console.error('Error fetching JSON:', error));
-      }
-  }, []);
+    useEffect(() => {
+        // ローカルサーバーからJSONデータを取得する
+        if (resultJsonData === null) {
+            fetch("http://localhost:50000/GET_RESULT", { method: "GET" })
+                .then((response) => response.json())
+                .then((data) => {
+                    setResultJsonData(data);
+                    console.log(data);
+                })
+                .catch((error) => console.error("Error fetching JSON:", error));
+        }
+        if (quizJsonData === null) {
+            fetch("http://localhost:50000/JSON", { method: "GET" })
+                .then((response) => response.json())
+                .then((data) => {
+                    setQuizJsonData(data);
+                    console.log("hogehoge");
+                    console.log(data);
+                })
+                .catch((error) => console.error("Error fetching JSON:", error));
+        }
+    }, []);
 
-  if (resultJsonData === null || quizJsonData === null) {
-    return
-  }
-  return (
-    <div className={styles.main}>
-      <div className={styles.title_bar}>
-        解答
-      </div>
-      <div className={styles.card}>
-        <div className={styles.content}>
-          <div className={styles.card_element}>#</div>
-          <div className={styles.card_element}>正誤</div>
-          <div className={styles.card_element}>あなたの回答</div>
-          <div className={styles.card_element}>正解</div>
-          <div className={styles.card_element}>問題</div>
-          <div className={styles.card_element}>解説</div>
+    if (resultJsonData === null || quizJsonData === null) {
+        return;
+    }
+    return (
+        <div className={styles.main}>
+            <div className={styles.title_bar}>解答</div>
+            <div className={styles.card}>
+                <div className={styles.content}>
+                    <div className={styles.card_element}>#</div>
+                    <div className={styles.card_element}>正誤</div>
+                    <div className={styles.card_element}>あなたの回答</div>
+                    <div className={styles.card_element}>正解</div>
+                    <div className={styles.card_element}>問題</div>
+                    <div className={styles.card_element}>解説</div>
+                </div>
+            </div>
+            <div>
+                {resultJsonData.map((elem: ResultJson, index: number) => (
+                    <div key={index}>
+                        <ResultArea
+                            num={index + 1}
+                            selected={
+                                quizJsonData[index].options[elem.Selected]
+                            }
+                            answer={quizJsonData[index].options[elem.Answer]}
+                            questionUrl={
+                                "http://localhost:50000/HTML/question/" +
+                                quizJsonData[index].file
+                            }
+                            answerUrl={
+                                "http://localhost:50000/HTML/answer/" +
+                                quizJsonData[index].file
+                            }
+                        />
+                    </div>
+                ))}
+            </div>
+            <div className={styles.end_button} onClick={goTitle}>
+                最初に戻る
+            </div>
         </div>
-      </div>
-      <div>
-        {resultJsonData.map((elem: ResultJson, index: number) => (
-          <div key={index}>
-            <ResultArea
-            num={index+1}
-            selected={quizJsonData[index].options[elem.Selected]}
-            answer={quizJsonData[index].options[elem.Answer]}
-            questionUrl={"http://localhost:50000/HTML/question/" + quizJsonData[index].file}
-            answerUrl={"http://localhost:50000/HTML/answer/" + quizJsonData[index].file} />
-          </div>
-        ))}
-      </div>
-      <div className={styles.end_button} onClick={goTitle}>
-        最初に戻る
-      </div>
-    </div>
-  )
+    );
 }
